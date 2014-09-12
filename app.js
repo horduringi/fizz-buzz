d36ce47d370b3be9bc761f0d764e083fe6707ccf
Hörður <horduringi@gmail.com>
2014-09-12 13:12:51 +0000

    FizzBuzz refactor

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
-	for(var i = 1; i < 101; i ++)
-	{
-		var str = "";
-		if (i % 3 === 0) {
-			str += "fizz";
+	do {
+		var num = +prompt("Give us an integer:");
+	}while(isNaN(num) || num % 1 != 0)
+	var fizzBuzz = function(num){
+		for(var i = 1; i <= num; i ++)
+		{
+			var str = "";
+			if (i % 3 === 0) {
+				str += "fizz";
+			}
+			if (i % 5 === 0){
+				str += "buzz";
+			}
+			if (!str){
+				str = i;
+			}
+			$("body").append("<p>" + str + "</p>")
 		}
-		if (i % 5 === 0){
-			str += "buzz";
-		}
-		if (!str){
-			str = i;
-		}
-		$("body").append("<p>" + str + "</p>")
 	}
+	fizzBuzz(num);
 })
